4ceb50ef71472bf9bd341158d665761d2c02a200
iZeroKim <kimgichuru6@gmail.com>
2022-09-05 23:05:46 +0300

    Binded RegisterActivity with the authViewModel

--- app/src/main/java/com/wasusi/k_cloud/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/wasusi/k_cloud/ui/auth/RegisterActivity.kt
@@ -1,12 +1,49 @@
 package com.wasusi.k_cloud.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.wasusi.k_cloud.R
+import com.wasusi.k_cloud.databinding.ActivityRegisterBinding
+import com.wasusi.k_cloud.util.startHomeActivity
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity(), AuthListener, KodeinAware {
+
+    override val kodein: Kodein by kodein()
+    private val authViewModelFactory: AuthViewModelFactory by instance()
+
+    private lateinit var authViewModel: AuthViewModel
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_register)
+        authViewModel = ViewModelProvider(this, authViewModelFactory).get(AuthViewModel::class.java)
+        binding.viewmodel = authViewModel
+
+        authViewModel.authListener = this
+    }
+
+
+
+    override fun onStarted() {
+        binding.progressbar.visibility = View.VISIBLE
+    }
+
+    override fun onSuccess() {
+        binding.progressbar.visibility = View.GONE
+        startHomeActivity()
+    }
+
+    override fun onFailure(message: String) {
+        binding.progressbar.visibility = View.GONE
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
