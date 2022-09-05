db8eb8459ee6fad995c315254d56de7e80e015ca
iZeroKim <kimgichuru6@gmail.com>
2022-09-05 16:22:21 +0300

    Initial color setup

--- app/src/main/java/com/wasusi/k_cloud/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/wasusi/k_cloud/ui/auth/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.wasusi.k_cloud.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wasusi.k_cloud.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
