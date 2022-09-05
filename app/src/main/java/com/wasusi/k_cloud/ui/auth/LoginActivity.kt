cd2efbe5a1e5a1f1d84490a37508381d09422b97
iZeroKim <kimgichuru6@gmail.com>
2022-09-05 15:31:58 +0300

    Login and Register Activities initial setup

---
+++ app/src/main/java/com/wasusi/k_cloud/ui/auth/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.wasusi.k_cloud.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/wasusi/k_cloud/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.wasusi.k_cloud.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wasusi.k_cloud.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
