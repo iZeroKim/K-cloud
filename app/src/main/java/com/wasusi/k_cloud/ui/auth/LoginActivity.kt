fc32daafa518d7a2f479c89beac566e61405ec4c
iZeroKim <kimgichuru6@gmail.com>
2022-09-05 22:41:15 +0300

    Minor fix - added data binding to build.gradle

--- app/src/main/java/com/wasusi/k_cloud/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/wasusi/k_cloud/ui/auth/LoginActivity.kt
@@ -1,12 +1,39 @@
 package com.wasusi.k_cloud.ui.auth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.wasusi.k_cloud.R
+import com.wasusi.k_cloud.databinding.ActivityLoginBinding
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.kodein
+import org.kodein.di.generic.instance
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), KodeinAware, AuthListener {
+    override val kodein by kodein()
+    private val authViewModelFactory: AuthViewModelFactory by instance()
+
+    private lateinit var authViewModel: AuthViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding: ActivityLoginBinding  = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        authViewModel = ViewModelProvider(this, authViewModelFactory).get(AuthViewModel::class.java)
+        binding.viewmodel = authViewModel
+
+        authViewModel.authListener = this
+    }
+
+    override fun onStarted() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSuccess() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onFailure(message: String) {
+        TODO("Not yet implemented")
     }
 }
