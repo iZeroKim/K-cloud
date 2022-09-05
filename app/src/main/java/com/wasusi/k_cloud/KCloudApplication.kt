ef78f38d12154ceae567fa80ef934b28287eb24f
iZeroKim <kimgichuru6@gmail.com>
2022-09-05 17:30:33 +0300

    Created dependency provider using kodein

---
+++ app/src/main/java/com/wasusi/k_cloud/KCloudApplication.kt
@@ -0,0 +1,24 @@
+package com.wasusi.k_cloud
+
+import android.app.Application
+import com.wasusi.k_cloud.data.firebase.Firebase
+import com.wasusi.k_cloud.data.repositories.UserRepository
+import com.wasusi.k_cloud.ui.auth.AuthViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+class KCloudApplication : Application(), KodeinAware{
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@KCloudApplication))
+        bind() from singleton { Firebase() }
+        bind() from singleton {UserRepository(instance())}
+        bind() from provider { AuthViewModelFactory(instance()) }
+
+    }
+
+}
