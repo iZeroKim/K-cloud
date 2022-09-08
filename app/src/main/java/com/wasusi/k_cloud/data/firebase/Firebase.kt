b85d9a330baec2fb301efdc4eaa595c3b63637c3
iZeroKim <kimgichuru6@gmail.com>
2022-09-08 21:06:31 +0300

    Added firestore dependency and initiated fetch folders functionality

--- app/src/main/java/com/wasusi/k_cloud/data/firebase/Firebase.kt
+++ app/src/main/java/com/wasusi/k_cloud/data/firebase/Firebase.kt
@@ -1,11 +1,16 @@
 package com.wasusi.k_cloud.data.firebase
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import io.reactivex.Completable
 
 class Firebase {
     private val auth: FirebaseAuth by lazy {
         FirebaseAuth.getInstance()
+    }
+    private val db by lazy{
+        Firebase.firestore
     }
 
     fun login(email: String, password: String) = Completable.create{emitter ->
@@ -35,4 +40,19 @@ class Firebase {
     fun logout() = auth.signOut()
 
     fun currentUser() = auth.currentUser
+
+    fun getFolders() = Completable.create{emitter ->
+        db.collection("folders")
+            .get()
+            .addOnCompleteListener {
+                if(!emitter.isDisposed){
+                    if(it.isSuccessful){
+                        emitter.onComplete()
+                    } else{
+                        emitter.onError(it.exception!!)
+                    }
+                }
+            }
+
+    }
 }
