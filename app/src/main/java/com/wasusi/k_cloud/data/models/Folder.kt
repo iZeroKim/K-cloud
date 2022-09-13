d86d51d0debd6b7d509267f92449ceea0fc8ba2f
iZeroKim <kimgichuru6@gmail.com>
2022-09-13 20:59:35 +0300

    Add Folder data class

---
+++ app/src/main/java/com/wasusi/k_cloud/data/models/Folder.kt
@@ -0,0 +1,8 @@
+package com.wasusi.k_cloud.data.models
+
+
+data class Folder(
+    val folderId:String,
+    val name:String,
+    val created_at:String
+)
--- app/src/main/java/com/wasusi/k_cloud/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/wasusi/k_cloud/ui/home/HomeViewModel.kt
@@ -2,13 +2,15 @@ package com.wasusi.k_cloud.ui.home
 
 import android.view.View
 import androidx.lifecycle.ViewModel
+import com.wasusi.k_cloud.data.repositories.FoldersRepository
 import com.wasusi.k_cloud.data.repositories.UserRepository
 import com.wasusi.k_cloud.util.startLoginActivity
 import io.reactivex.disposables.CompositeDisposable
 
 class HomeViewModel(
-    private val repository: UserRepository
-): ViewModel() {
+    private val repository: UserRepository,
+    private val foldersRepository: FoldersRepository
+): ViewModel(){
     private val disposables = CompositeDisposable()
 
 
@@ -16,9 +18,14 @@ class HomeViewModel(
         repository.currentUser()
     }
 
+    fun fetchFolders(){
+        foldersRepository.fetchFolders()
+    }
+
     fun logout(view: View){
         repository.logout()
         view.context.startLoginActivity()
 
     }
+
 }
