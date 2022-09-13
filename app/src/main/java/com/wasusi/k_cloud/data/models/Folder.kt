9ddaa6dbb176e61d4be316f7cbc79639693bb579
iZeroKim <kimgichuru6@gmail.com>
2022-09-13 21:00:29 +0300

    Replace default DocumentSnapshot toObject() with a custom one

--- app/src/main/java/com/wasusi/k_cloud/data/models/Folder.kt
+++ app/src/main/java/com/wasusi/k_cloud/data/models/Folder.kt
@@ -1,8 +1,25 @@
 package com.wasusi.k_cloud.data.models
 
+import android.util.Log
+import com.google.firebase.firestore.DocumentSnapshot
+
 
 data class Folder(
     val folderId:String,
     val name:String,
     val created_at:String
-)
+){
+    companion object{
+        fun DocumentSnapshot.toFolder(): Folder?{
+            try{
+                val name = getString("name")!!
+                val created_at = getString("created_at")!!
+                return Folder(id, name, created_at)
+            } catch (e: Exception){
+                Log.e("Folder", "Error converting fetched folders", e)
+                return null
+            }
+        }
+    }
+
+}
