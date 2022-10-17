3520ecf3f2bef2d173d157ffb31905764cc21b89
iZeroKim <kimgichuru6@gmail.com>
2022-10-17 15:49:59 +0300

    Add fetch file util

---
+++ app/src/main/java/com/wasusi/k_cloud/util/FetchFiles.kt
@@ -0,0 +1,14 @@
+package com.wasusi.k_cloud.util
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+fun Context.fetchFile(type: String){
+    val REQUEST_CODE = 100
+
+    val intent = Intent(Intent.ACTION_PICK)
+    intent.type = type
+    startActivityForResult(this as Activity, intent, REQUEST_CODE, null)
+}
