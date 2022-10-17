a66ab73342fb9ebb883f0b6fe015d768eeaeab54
iZeroKim <kimgichuru6@gmail.com>
2022-10-17 15:51:52 +0300

    Get file uri

--- app/src/main/java/com/wasusi/k_cloud/ui/folders/FolderDetailsActivity.kt
+++ app/src/main/java/com/wasusi/k_cloud/ui/folders/FolderDetailsActivity.kt
@@ -1,6 +1,9 @@
 package com.wasusi.k_cloud.ui.folders
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.wasusi.k_cloud.R
 
@@ -18,9 +21,13 @@ class FolderDetailsActivity : AppCompatActivity() {
 
 
     }
-
-
-
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        var REQUEST_CODE = 100
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            // handle chosen image
+            Log.i("uri", data?.data.toString())
+        }
+    }
 
 }
