22b1ce13bf9ad8895136b46a1e58fc7d94a240aa
iZeroKim <kimgichuru6@gmail.com>
2022-11-15 12:27:18 +0300

    Create months list adapter

---
+++ app/src/main/java/com/wasusi/k_cloud/util/adapters/MonthsAdapter.kt
@@ -0,0 +1,47 @@
+package com.wasusi.k_cloud.util.adapters
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.wasusi.k_cloud.R
+import com.wasusi.k_cloud.ui.folders.FolderDetailsActivity
+
+class MonthsAdapter(val months: List<String>) :
+    RecyclerView.Adapter<MonthsAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.single_month_layout, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.month.text =months[position]
+
+        /**
+         * Start folders detail activity onclick of folder
+         * Pass folder name as an intent bundle
+         */
+        holder.itemView.setOnClickListener {
+            it.context.startActivity(
+                Intent(it.context, FolderDetailsActivity::class.java).apply {
+                    putExtra("current_month", months[position] as java.io.Serializable)
+                }
+            )
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return months.size
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val month = itemView.findViewById<TextView>(R.id.title)
+
+    }
+
+
+}
